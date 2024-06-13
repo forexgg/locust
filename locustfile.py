a6f239d1a75d89a2db0271ead18003327a81229f
Steven Zhang <stevenz@whitehardt.com>
2024-06-12 21:52:24 -0500

    - test with POST request, different task priority and wait time

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,22 @@
-from locust import HttpUser, task
+from time import sleep
+from locust import HttpUser, task, between
 
-class HelloWorldUser(HttpUser):
+
+class QuickStartUser(HttpUser):
+    wait_time = between(1, 5)
+
     @task
     def hello_world(self):
         self.client.get("/")
         self.client.get("/about")
-        self.client.get("/random")
+
+    @task(3)
+    def view_items(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
+            sleep(1)
+
+    def on_start(self):
+        self.client.post(
+            "/login", json={"username": "steven", "password": "supersecret"}
+        )
--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from random import randint
 from time import sleep
 
@@ -20,6 +20,20 @@ def about():
 def rndm():
     sleep(randint(0, 2))
     return "OK"
+
+
+@app.route("/item")
+def items():
+    item_id = request.args.get("id", None)
+    return f"OK - {item_id}"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return "OK_POST"
+    else:
+        return "OK_GET"
 
 
 if __name__ == "__main__":
